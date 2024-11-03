61ecf6d31c1fadabb33bbb80e4198b321662dfa8
gagandeep1763 <gaganraj167@gmail.com>
2024-11-03 22:07:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import ColorPicker from "./colorpicker.jsx"; // Ensure the imported name is capitalized
+
+function App() {
+  return (
+    <>
+      <ColorPicker /> {/* Use uppercase for component */}
+    </>
+  );
+}
+
+export default App;
